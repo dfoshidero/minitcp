e06f22ed1c16c4a55f7743dd632316377719f5d5
dfoshidero <dfoshidero@outlook.com>
2026-08-16 22:46:11 +0000

    fix: expose MacAddress bytes for ARP replies
    ARP needs to copy MiniTCP's MAC onto the wire from other modules.

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -1,7 +1,7 @@
 // src/ethernet.rs
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct MacAddress([u8; 6]);
+pub struct MacAddress(pub [u8; 6]);
 
 impl std::fmt::Display for MacAddress {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
